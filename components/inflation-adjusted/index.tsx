4f3c5bfbb38314665c36438cd0756130d0cf622b
Catriel Müller <catrielmuller@gmail.com>
2023-06-07 09:06:50 -0300

    Sueldos

---
+++ components/inflation-adjusted/index.tsx
@@ -0,0 +1,26 @@
+
+type InflationAdjustedProps = {
+  value: string
+  date: string
+}
+
+const ANUAL_INFLATION = 108.8
+const DAILY_INFLATION = ANUAL_INFLATION / 365
+const MILISECONDS_IN_DAY = 1000 * 60 * 60 * 24
+
+const ARSFormatter = new Intl.NumberFormat('es-AR', {
+  style: 'currency',
+  currency: 'ARS',
+})
+
+export const InflationAdjusted = (props: InflationAdjustedProps) => {
+  const { value, date } = props
+
+  const parsedDate = new Date(date)
+  const parsedValue = parseFloat(value)
+  const now = new Date()
+  const diff = Math.trunc((now.getTime() - parsedDate.getTime()) / MILISECONDS_IN_DAY)
+  const adjustedValue = parsedValue + ((parsedValue * (diff * DAILY_INFLATION)) / 100)
+
+  return ARSFormatter.format(adjustedValue)
+}
